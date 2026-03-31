3ade6e1ee8a89e0a16eadafab32ab8da9038bb06
Rishithgoud <23211a6607@gmail.com>
2026-03-31 17:23:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#SPDX-License-Identifier: MIT
 from flask import Flask, render_template, request, redirect, session, jsonify
 import sqlite3, os, datetime
 from textblob import TextBlob
@@ -288,7 +289,6 @@ RECOMMENDED ACTION:
 Focused administrative review and local-level monitoring are advised
 to prevent further public dissatisfaction and implementation risks.
 """.strip()
-SPDX-License-Identifier: MIT
     # ---- ISSUE COUNT ----
     issue_count = {}
     for f in feedback_rows:
